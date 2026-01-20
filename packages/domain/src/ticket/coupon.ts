d4584b20316d0d8975740213503e5b9c564626a2
Bakate <bakateba@gmail.com>
2026-01-20 10:55:38 +0100

    feat(domain): implement ticketing aggregate

---
+++ packages/domain/src/ticket/coupon.ts
@@ -0,0 +1,14 @@
+import { Schema } from "effect";
+import { FlightId } from "../flight-entity/flight.js";
+
+export class Coupon extends Schema.Class<Coupon>("Coupon")({
+	couponNumber: Schema.Number.pipe(Schema.int(), Schema.positive()),
+	flightId: FlightId,
+	seatNumber: Schema.OptionFromNullOr(Schema.String),
+	status: Schema.optionalWith(
+		Schema.Literal("OPEN", "USED", "VOID", "EXCHANGED", "CHECKED_IN"),
+		{
+			default: () => "OPEN",
+		},
+	),
+}) {}
